9351138cab921091ba2ad799ad70475605bf6453
Bruno Ferreira <contatobrunokye@gmail.com>
2023-07-12 18:18:30 -0300

    feat: requisito 05

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from typing import Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None):
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: list[Product]) -> None:
+        return self._data.extend(data)
